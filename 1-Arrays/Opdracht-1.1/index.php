144b32cb6a2c7fc8f1357db48197582b81539b6a
Lucien Osei <lucien.osei@student.rocva.nl>
2022-02-15 13:03:10 +0100

    opdracht 1

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Lucien";
+$student['achternaam'] = "Osei";
+$student['leeftijd'] = 17;
+$student['klas'] = '1D';
+
+echo "Gegevens Lucien: Voornaam:". " " . " " . $student['voornaam'] . " " . "Achternaam:" . $student['achternaam'] . " " . "Leeftijd:" . " " . $student['leeftijd'] . " ". "Klas:" . " " . $student['klas'];
+?>
