ec43ddff6488edd6be2d7a6d55ec8c5577988ede
Lucien Osei <lucien.osei@student.rocva.nl>
2022-02-16 07:50:05 +0100

    op 1.2

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $persoon["voornaam"] = "Lucien";
+    $persoon["achternaam"] = "Osei";
+    $persoon["leeftijd"] = 17;
+    $persoon["opleiding"] = "ICT";
+
+    echo $persoon['voornaam']. " " . $persoon['achternaam'] . " is " . $persoon['leeftijd'] . " jaar oud en is student aan het ROC vam Amsterdam aan de opleiding" . " " . $persoon['opleiding'] . "."; 
+    
+?>
