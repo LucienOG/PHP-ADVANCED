fdeb7e62dc70bedc3bf2e215809aeb8ac766fa21
Lucien Osei <lucien.osei@student.rocva.nl>
2022-03-10 11:25:30 +0100

    3.1 af

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+        <?php echo $_GET["voornaam"]; ?><br>
+        <?php echo $_GET["achternaam"]; ?><br>
+        <?php echo $_GET["klas"]; ?><br>
+        <?php echo $_GET["leeftijd"]; ?><br>
+        <?php echo $_GET["adres"]; ?><br>
+        <?php echo $_GET["plaatsnaam"]; ?><br>
+</body>
+</html>
