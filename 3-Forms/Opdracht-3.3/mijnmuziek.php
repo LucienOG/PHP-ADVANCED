c6ad97ec51407458b0ef4f9b2ea393488295f385
Lucien Osei <lucien.osei@student.rocva.nl>
2022-03-15 11:53:38 +0100

    muziek.php

---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>Joyner Lucas & Lil Baby</li>
+        <li>Ramen & OJ</li>
+        <li>2021</li>
+        <li>Rap</li>
+    </ul>
+</body>
+</html>
