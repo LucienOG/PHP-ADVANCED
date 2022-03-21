c3c5f45959bf59c284addb5604996f3d03b9599e
Lucien Osei <lucien.osei@student.rocva.nl>
2022-03-21 11:01:47 +0100

    4.2

---
+++ 4-Functions/Opdracht-4.2/functies-2.php
@@ -0,0 +1,11 @@
+<?php
+    function rekenUit($getal1, $getal2, $getal3) {
+        $uitkomst = $getal1 + $getal2 + $getal3;
+        return $uitkomst;
+    }
+
+    echo "Als ik de getallen 1, 1 en 1 bij elkaar optel, is de uitkomst ". rekenUit(1, 1, 1,)."<br>";
+    echo "Als ik de getallen 2, 2 en 2 bij elkaar optel, is de uitkomst ". rekenUit(2, 2, 2,)."<br>";
+    echo "Als ik de getallen 3, 3 en 3 bij elkaar optel, is de uitkomst ". rekenUit(3, 3, 3,)."<br>";
+
+?>
