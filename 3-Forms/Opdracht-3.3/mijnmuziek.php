bd2c3084b2578a0d64a3a6e7a474e4966611a690
Lucien Osei <lucien.osei@student.rocva.nl>
2022-03-21 10:35:41 +0100

    4.1

--- 3-Forms/Opdracht-3.3/mijnmuziek.php
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -7,11 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        <li>Joyner Lucas & Lil Baby</li>
-        <li>Ramen & OJ</li>
-        <li>2021</li>
-        <li>Rap</li>
-    </ul>
+    <form name="mijnmuziek" method="GET">
+        <ul>
+            <li>Artiest: <input name="artiest" value="<?php echo $_GET ["artiest"]?>"></li>
+            <li>Titel: <input name="titel" value="<?php echo $_GET ["titel"]?>"></li>
+            <li>Jaar van uitkomst: <input name="uitkomst" value="<?php echo $_GET ["uitkomst"]?>" ></li>
+            <li>Muzieksoort: <input name="muzieksoort" value="<?php echo $_GET ["muzieksoort"]?>" ></li>
+        </ul>
+
+        <input type="submit" name="submit" value="Verstuur">
+        <!-- http://localhost/PHP-ADVANCED/3-Forms/Opdracht-3.3/mijnmuziek.php?artiest=joynerLucas&titel=ADHD&uitkomst=2021&muzieksoort=rap -->
+    <form>
 </body>
 </html>
